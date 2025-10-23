69a246f9838bdad1908a08cc565e8b9e57be5314
OrunbekK <orunbek@gmail.com>
2025-10-23 21:23:14 +0600

    init

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,38 @@
 // src/main.ts
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
-import Lara from '@primevue/themes/lara'
+
+import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes'
+
 import 'primeicons/primeicons.css'
 import './styles/main.css'
 
 import App from './App.vue'
 import router from './router'
 
+const AuraBlue = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50:  '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+        }
+    }
+})
+
 const app = createApp(App)
 
 app.use(PrimeVue, {
     theme: {
-        preset: Lara,
+        preset: AuraBlue,
         options: {
             prefix: 'p',
             darkModeSelector: '.app-dark',
